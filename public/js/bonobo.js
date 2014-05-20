d79f6de73787110f8a927501f39733cebd6dd608
Michael Lewis <mike@lew42.com>
2014-05-19 20:44:24 -0500

    Initial commit

---
+++ public/js/bonobo.js
@@ -0,0 +1,175 @@
+;(function($, _){
+
+	if (typeof MPL !== "object")
+		MPL = {};
+
+	var Property = MPL.Property = function Property(name){
+		var self = this;
+		this.name = name;
+		this.getValue = function(){
+			return self._value;
+		};
+
+		this.setValue = function(newValue){
+			self._value = newValue;
+			self.type = typeof newValue;
+			self.trigger('change');
+		};
+
+		this.getType = function(){
+			return self.type;
+		};
+
+		this.hasProperty = function(prop){
+			return typeof self.getValue()[prop] !== 'undefined';
+		};
+
+		this.getProperty = function(prop){
+			return self.hasProperty(prop) && self.getValue()[prop];
+		};
+
+		this.bind = function(bindTo){
+			// this would be different for object properties.  Bonobo is the object property?
+			// should Property be allowed for objects and literals?
+			// should Bonobo extend from Property?
+			// .prop('propName') to get the Property, instead of getProperty?
+			if (self.getValue())
+
+
+			property.on('change', function(){
+				self.setValue(property.getValue());
+			});
+		};
+	};
+
+	Property.prototype = Object.create(MPL.Events.prototype);
+	Property.prototype.constructor = MPL.Events;
+	Property.prototype.valueOf = function(){
+		return this._value;
+	};
+
+
+	var Bonobo = MPL.Bonobo = function Bonobo(){
+		this._properties = [];
+	};
+
+	Bonobo.prototype = Object.create(MPL.Events.prototype);
+	Bonobo.prototype.constructor = MPL.Bonobo;
+
+	Bonobo.prototype.addProperty = function(prop){
+		var property = new Property(prop);
+
+		Object.defineProperty(this, prop, {
+			get: property.getValue,
+			set: property.setValue
+		});
+
+		this._properties[prop] = property;
+	};
+
+	Bonobo.prototype.isSet = function(prop){
+		return typeof this[prop] !== 'undefined';
+	};
+
+	Bonobo.prototype.hasProperty = function(prop){
+		return typeof this._properties[prop] !== 'undefined';
+	};
+
+	Bonobo.prototype.isProperty = function(prop){
+		return this._properties[prop] instanceof Property;
+	};
+
+	Bonobo.prototype.getProperty = function(prop){
+		if (this.hasProperty(prop) && this.isProperty(prop))
+			return this._properties[prop];
+		else
+			return undefined;
+	};
+
+	Bonobo.prototype.bind = function(prop, bindTo){
+		var property;
+		if (property = this.getProperty(prop)){
+			// push property's changes to bindTo
+			property.on('change', function(){
+				bindTo.setValue(property.getValue());
+			});
+
+			// for bindTo's changes, check what it is
+			var event;
+			if (isProperty(bindTo)){
+				event = 'on';
+			} else if (bindTo instanceof jQuery){
+				event = 'keypress';
+			}
+
+			// push bindTo's changes to property
+			bindTo.on(event, function(){
+				property.setValue(bindTo.getValue());
+			});
+
+			// start syncd
+			bindTo.setValue(property.getValue());
+		}
+	};
+
+	$.fn.getValue = function(){
+		var $first = this.first();
+		return $first.html() || $first.val();
+	};
+
+	$.fn.setValue = function(value){
+		return this.each(function(){
+			var $self = $(this);
+			$self.html(value);
+			if ($self.html() != value){
+				$self.val(value);
+				if ($self.val() != value){
+					console.log('Could not $().setValue()');
+				}
+			}
+		});
+	};
+
+	$.fn.bind = function($el){
+		return this.each(function(){
+			var $self = $(this),
+				oldValue = $self.getValue();
+
+			$self.on('keydown keyup change', function(){
+				$el.setValue($self.getValue());
+			});
+			$el.on('keydown keyup change', function(){
+				$self.setValue($el.getValue());
+			});
+			$el.setValue($self.getValue());
+		});
+	};
+
+	var isProperty = function(prop){
+		return prop instanceof Property;
+	};
+
+	/*
+	obj.is('propName').true() // obj.propName === true
+	obj.is('propName').truthy() // obj.propName == true // obj.propName
+	obj.is('propName').defined() // typeof obj.propName !== 'undefined'
+ ----->!!! This will make logging/testing really easy.  ENCAPSULATED, OO JAVASCRIPT LOGIC
+
+
+	Learn by example:
+	2people // BAD VARIABLE NAME
+
+	obj.Lastpropforperson // BAD CAMEL CASE
+
+	 */
+
+
+	var View = MPL.View = function View(){
+
+	};
+
+	View.prototype = Object.create(MPL.Events.prototype);
+	View.prototype.constructor = MPL.View;
+
+
+})(jQuery, _);
---
+++ public/js/html.js
@@ -0,0 +1,113 @@
+;(function($, _){
+
+	if (typeof MPL !== "object")
+		MPL = {};
+
+	var Attribute = MPL.Attribute = function Attribute(name, value){
+		MPL.Bonobo.call(this);
+		this.initialize(name, value);
+	};
+
+	Attribute.prototype = Object.create(MPL.Bonobo.prototype);
+	Attribute.prototype.constructor = MPL.Attribute;
+	Attribute.prototype.initialize = function(name, value){
+		this.addProperty('name');
+		this.name = name;
+		this.addProperty('value');
+		this.value = value;
+		this.view = new AttributeView(this);
+	};
+
+	var AttributeView = MPL.AttributeView = function AttributeView(attribute){
+		this.attribute = attribute;
+		this.$el = $('#attribute-view').clone().attr('id', null).removeClass('template');
+		// these could be auto-initializers also...  If more than one of a class is found, make an array
+		// this['$'+name] = this.$el.find('.'+name);
+		this.$name = this.$el.find('.name').edit();
+		this.$value = this.$el.find('.value').edit();
+
+		this.attribute.bind('name', this.$name);
+		this.attribute.bind('value', this.$value);
+	};
+
+	AttributeView.prototype = Object.create(MPL.View.prototype);
+	AttributeView.prototype.constructor = MPL.AttributeView;
+
+	var Element = MPL.Element = function Element(tag){
+		MPL.Bonobo.call(this);
+		this.initialize(tag);
+	};
+
+	Element.prototype = Object.create(MPL.Bonobo.prototype);
+	Element.prototype.constructor = MPL.Element;
+	Element.prototype.initialize = function(tag){
+		this.addProperty('tag');
+		this.tag = tag;
+		this.attributes = [];
+
+		this.view = new ElementView(this);
+		this.view.$el.appendTo('body');
+
+		var element = this;
+		this.$addAttributeBtn = $('#add-attribute-view').clone().attr('id', null).removeClass('template').click(function(){
+			element.addAttribute('name', 'value');
+		});
+		this.$addAttributeBtn.appendTo(this.view.$attributes);
+	};
+	Element.prototype.addAttribute = function(name, value){
+		var newAttribute = new Attribute(name, value);
+		newAttribute.view.$el.appendTo(this.view.$attributes);
+		this.$addAttributeBtn.appendTo(this.view.$attributes);
+		this.attributes.push(newAttribute);
+	};
+
+	var ElementView = MPL.ElementView = function ElementView(element){
+		this.element = element;
+		this.$el = $('#element-view').clone().attr('id', null).removeClass('template');
+		this.$tag = this.$el.find('.tag');
+		this.$attributes = this.$el.find('.attributes');
+
+		// this could be a 'standard bindings' loop... bind('value', this['$'+value]
+		this.element.bind('tag', this.$tag);
+	};
+
+	ElementView.prototype = Object.create(MPL.View.prototype);
+	ElementView.prototype.constructor = MPL.ElementView;
+
+	var AddAttributeBtn = MPL.AddAttributeBtn = function AddAttributeBtn(element){
+		this.view = {};
+		this.view.$el = $('#add-attribute-view').clone().attr('id', null).removeClass('template').click(function(){
+
+		});
+	}
+
+	var isProperty = function(prop){
+		return prop instanceof Property;
+	};
+
+	var focus = function(){
+		console.log('focus');
+		setTimeout(function(){
+			document.execCommand('selectAll', false, null);
+		}, 0);
+	};
+	$.fn.edit = function(end){
+		/* if (end || end == 'end'){
+			return this.off('focus', focus).attr('contenteditable', null);
+		}
+		return this.off('focus', focus).attr('contenteditable', true).on('focus', focus).focus(); */
+		var clickFunc = function(){
+			$(this).attr('contenteditable', true).off('click', clickFunc).blur(function(){
+				$(this).attr('contenteditable', null).on('click', clickFunc);
+			});
+		};
+		return this.on('click', clickFunc);
+	};
+
+	$.fn.hoverEdit = function(){
+		return this.click(function(){
+				$(this).edit();
+			});
+	};
+
+})(jQuery, _);
